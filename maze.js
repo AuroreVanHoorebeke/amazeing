744d43fef97c58231f28ff949b253cf55f7ed836
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-11-23 12:16:19 +0100

    split array, individual div for each sign

--- maze.js
+++ maze.js
@@ -1,6 +1,29 @@
 const main = document.querySelector("main");
 
-const tileDiv = document.createElement("div");
-tileDiv.className = "tile";
-main.appendChild(tileDiv);
+// const tileDiv = document.createElement("div");
+// tileDiv.className = "tile";
+// main.appendChild(tileDiv);
 
+// const lineDiv = document.createElement("div");
+// lineDiv.className = "line";
+// main.appendChild(lineDiv);
+
+const wallTile = document.createElement("div");
+wallTile.className = "wall";
+main.appendChild(wallTile);
+
+const pathTile = document.createElement("div");
+pathTile.className = "path";
+main.appendChild(pathTile);
+
+const multiline = `***********.**S.....**.*.T*****.....*.******.***.*.******.*****.******.*****.******.......******.********.........****.******...***....********`;
+
+console.log(multiline);
+
+for (let i = 0; i < multiline.length; i++) {
+    const tileDiv = document.createElement("div");
+    tileDiv.className = "tile";
+    main.appendChild(tileDiv);
+
+    tileDiv.textContent = multiline[i].split('');// ['l', 'i', 'n', 'e', '1']
+}
